c1e9b0b3dc91bc17332e3e66edeef49c6d8c23ae
Enricalo <73693992+Enricalo@users.noreply.github.com>
2020-11-23 12:29:29 +0100

    Update R_code_point_pattern_analysis.r

--- R_code_point_pattern_analysis.r
+++ R_code_point_pattern_analysis.r
@@ -19,7 +19,56 @@ density_map<-density(covid_planar)
 plot(density_map)
 
 #change colors (100 represent how many colors are in the palette)
-cl <- colorRampPalette(c('white','blue','purprle'))(100)
+cl <- colorRampPalette(c('blue','yellow','orange','red','magenta'))(100) 
 plot(density_map, col = cl)
 points(covid_planar)
 #all potential colors in http://www.stat.columbia.edu/~tzheng/files/Rcolor.pdf
+
+#install rgdal
+install.packages("rgdal")
+library(rgidal)
+
+#data from the folder + overlap the colored graph to the coastline drawing
+coastlines <- readOGR("ne_10m_coastline.shp")
+plot(density_map, col = cl)
+points(covid_planar)
+plot(coastlines, add = TRUE)
+
+#colors and graph
+cl <- colorRampPalette(c('pink','green','orange','red','magenta'))(100) # 
+plot(density_map, col = cl)
+points(covid_planar, pch = 19)
+plot(coastlines, add = TRUE)
+
+#create and save graph as imagine or pdf -> it will save the file in lab folder
+png("figure1.png")
+cl <- colorRampPalette(c('pink','green','orange','red','magenta'))(100) #
+plot(density_map, col = cl)
+points(covid_planar, pch = 19, cex = 0.5)
+plot(coastlines, add = TRUE)
+dev.off()
+
+#pdf
+pdf("figure1.pdf")
+cl <- colorRampPalette(c('pink','green','orange','red','magenta'))(100) # 
+plot(density_map, col = cl)
+points(covid_planar, pch = 19, cex = 0.5)
+plot(coastlines, add = TRUE)
+dev.off()
+
+#function ggsave to save the file
+ggsave("figure1.pdf")
+cl <- colorRampPalette(c('pink','green','orange','red','magenta'))(100) # 
+plot(density_map, col = cl)
+points(covid_planar, pch = 19, cex = 0.5)
+plot(coastlines, add = TRUE)
+dev.off()
+
+#analyse amount of cases instead of density
+marks(covid_planar) <- cases
+cases_map <- Smooth(covid_planar)
+plot(cases_map, col = cl)
+plot(coastlines, add = T)
+points(covid_planar)
+
+
